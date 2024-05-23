5073da9b4fab24ae284dec7f7e12d58e479ce587
Sinfinka <inadaradar@gmail.com>
2024-05-23 13:08:16 +0300

    add updates

--- services/contactsServices.js
+++ services/contactsServices.js
@@ -7,9 +7,9 @@ export const getContactById = (id, owner) => Contact.findOne(id, owner);
 export const addContact = (data) => Contact.create(data);
 
 export const editContact = (id, data) =>
-  Contact.findByIdAndUpdate(id, data, { new: true });
+  Contact.findOneAndUpdate(id, data, { new: true });
 
-export const removeContact = async (id) => Contact.findByIdAndDelete(id);
+export const removeContact = async (id) => Contact.findOneAndDelete(id);
 
 export const updateStatusContact = (id, data) =>
-  Contact.findByIdAndUpdate(id, data, { new: true });
+  Contact.findOneAndUpdate(id, data, { new: true });
